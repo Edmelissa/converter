4f72dfd4e8d008caa8de4a6e9cf45a8f5a9f8742
Edmelissa <101089613+Edmelissa@users.noreply.github.com>
2023-11-16 14:24:38 +0300

    refactoring

--- app/src/main/java/com/example/onverter/MainActivity.kt
+++ app/src/main/java/com/example/onverter/MainActivity.kt
@@ -5,149 +5,68 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
-import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
-    fun convert10to16(text10 : String) : String{
-        var int10 = text10.toLong()
-
-        var text16 = ""
-
-        while(int10 > 0){
-            val t = (int10 % 16)
-
-            if(t < 10){
-                text16 = t.toString() + text16
-            }
-            else{
-                when(t){
-                    10.toLong() -> text16 = 'A' + text16
-                    11.toLong() -> text16 = 'B' + text16
-                    12.toLong() -> text16 = 'C' + text16
-                    13.toLong() -> text16 = 'D' + text16
-                    14.toLong() -> text16 = 'E' + text16
-                    15.toLong() -> text16 = 'F' + text16
-                }
-            }
-            int10 = int10/16
-        }
-        return text16
+    private fun convert10to16(long10 : Long) : String{
+        return long10.toString(16).uppercase()
     }
 
-    fun convert16to10(text16 : String) : String{
-        var _text16 = text16
-        var int10 = 0.0
+    private fun convert16to10(text16 : String) : Long?{
+        return  text16.toLongOrNull(16)
+    }
 
-        var i = 0
-        while (_text16 != ""){
-            val t = _text16.substring(_text16.length - 1)
-
-            val regex = Regex("[0-9]")
-            if(t.matches(regex)){
-                int10 += t.toLong() * (16.0).pow(i)
-            }
-            else{
-                when(t){
-                    "A" -> int10 += 10 * (16.0).pow(i)
-                    "B" -> int10 += 11 * (16.0).pow(i)
-                    "C" -> int10 += 12 * (16.0).pow(i)
-                    "D" -> int10 += 13 * (16.0).pow(i)
-                    "E" -> int10 += 14 * (16.0).pow(i)
-                    "F" -> int10 += 15 * (16.0).pow(i)
-                }
-            }
-            _text16 = _text16.substring(0, _text16.length - 1)
-            i++
-        }
-
-        if(int10.toFloat() <= Long.MAX_VALUE) {
-            return int10.toLong().toString()
-        }
-        else{
-            return ""
-        }
+    private fun createAlertDialog(text : String){
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder
+            .setMessage(text)
+            .setTitle("Ошибка")
+            .setPositiveButton("ОК", { dialog, id ->  dialog.cancel() })
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val et10 = findViewById<EditText>(R.id.et10)
-        val et16 = findViewById<EditText>(R.id.et16)
+        val editText10 = findViewById<EditText>(R.id.editText10)
+        val editText16 = findViewById<EditText>(R.id.editText16)
 
-        val butConvert10to16 = findViewById<Button>(R.id.buttonConvert10to16)
-        val butConvert16to10 = findViewById<Button>(R.id.buttonConvert16to10)
+        val buttonConvert10To16 = findViewById<Button>(R.id.buttonConvert10To16)
+        val buttonConvert16To10 = findViewById<Button>(R.id.buttonConvert16To10)
+
+        val errorIncorrectValue = getResources().getString(R.string.error_incorrect_value)
 
         val test = true
         if(test) {
-            et10.setText(Long.MAX_VALUE.toString(), TextView.BufferType.EDITABLE)
-            et16.setText(convert10to16(Long.MAX_VALUE.toString()), TextView.BufferType.EDITABLE)
+            editText10.setText(Long.MAX_VALUE.toString())
+            editText16.setText(convert10to16(Long.MAX_VALUE))
         }
 
-        butConvert10to16.setOnClickListener { view ->
-            val text10 = et10.text.toString()
-            val regex = Regex("([0-9])+")
+        buttonConvert10To16.setOnClickListener {
+            val long10 = editText10.text.toString().toLongOrNull()
 
-            if(text10.matches(regex) && text10.toFloat() <= Long.MAX_VALUE ) {
-                et16.setText(convert10to16(text10), TextView.BufferType.EDITABLE)
-            }
-            else if(text10.toFloat() > Long.MAX_VALUE){
-                val builder: AlertDialog.Builder = AlertDialog.Builder(this)
-                builder
-                    .setMessage("Введено большое число")
-                    .setTitle("Ошибка")
-                    .setPositiveButton("ОК", { dialog, id ->  dialog.cancel() })
-                val dialog: AlertDialog = builder.create()
-                dialog.show()
-
-                Log.d("","Введено большое число")
-            }
-            else{
-                val builder: AlertDialog.Builder = AlertDialog.Builder(this)
-                builder
-                    .setMessage("Введено некорректное число")
-                    .setTitle("Ошибка")
-                    .setPositiveButton("ОК", { dialog, id ->  dialog.cancel() })
-                val dialog: AlertDialog = builder.create()
-                dialog.show()
-
-                Log.d("","Введено некорректное число")
-            }
-        }
-
-        butConvert16to10.setOnClickListener { view ->
-            val text16 = et16.text.toString().uppercase()
-            val regex = Regex("([0-9ABCDEF])+")
-
-            if(text16.matches(regex)) {
-                val text10 = convert16to10(text16)
-                if(text10 != "") {
-                    et10.setText(convert16to10(text16), TextView.BufferType.EDITABLE)
-                }
-                else{
-                    val builder: AlertDialog.Builder = AlertDialog.Builder(this)
-                    builder
-                        .setMessage("Введено большое число")
-                        .setTitle("Ошибка")
-                        .setPositiveButton("ОК", { dialog, id ->  dialog.cancel() })
-                    val dialog: AlertDialog = builder.create()
-                    dialog.show()
-
-                    Log.d("","Введено большое число")
-                }
+            if(long10 != null) {
+                editText16.setText(convert10to16(long10))
             }
             else {
-                val builder: AlertDialog.Builder = AlertDialog.Builder(this)
-                builder
-                    .setMessage("Введено некорректное число")
-                    .setTitle("Ошибка")
-                    .setPositiveButton("ОК", { dialog, id ->  dialog.cancel() })
-                val dialog: AlertDialog = builder.create()
-                dialog.show()
+                createAlertDialog(errorIncorrectValue)
+                Log.d("IncorrectValue",errorIncorrectValue)
+            }
+        }
 
-                Log.d("","Введено некорректное число")
+        buttonConvert16To10.setOnClickListener {
+            val text16 = editText16.text.toString().uppercase()
+
+            val long10 = convert16to10(text16)
+
+            if(long10 != null) {
+                editText10.setText(long10.toString())
+            }
+            else {
+                createAlertDialog(errorIncorrectValue)
+                Log.d("IncorrectValue",errorIncorrectValue)
             }
         }
     }
